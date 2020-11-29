6c17286433a32e038b33bf56f79e6c2129b8e940
abhiktech <abhikmaz@gmail.com>
2020-11-29 15:10:37 -0800

    Final commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 // Method override
-app.use(methodOverride(function (req, res) {
+app.use(methodOverride((req, res) => {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
        // look in urlencoded POST bodies and delete it
        let method = req.body._method
